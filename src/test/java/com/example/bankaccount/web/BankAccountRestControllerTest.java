32a61f145c8365dbf7409797a334e0889f95fd06
Lions <jeck000005@gmail.com>
2018-09-23 00:58:26 +0300

    some bugs fixed and tests started

--- src/test/java/com/example/bankaccount/web/BankAccountRestControllerTest.java
+++ src/test/java/com/example/bankaccount/web/BankAccountRestControllerTest.java
@@ -1,27 +1,50 @@
 package com.example.bankaccount.web;
 
+import com.example.bankaccount.model.BankAccount;
 import com.example.bankaccount.service.BankAccountService;
+import com.example.bankaccount.util.exception.NotFoundException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(BankAccountRestController.class)
+//@WebMvcTest(controllers = BankAccountRestController.class)
+@SpringBootTest
 public class BankAccountRestControllerTest {
 
-    @Autowired
+    //@Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    WebApplicationContext context;
+
     @MockBean
-    private BankAccountService service;
+    private BankAccountService serviceMock;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
     @Test
     public void createWithLocation() {
@@ -37,6 +60,13 @@ public class BankAccountRestControllerTest {
 
     @Test
     public void balance() throws Exception {
+        when(serviceMock.getById(1000)).thenReturn(new BankAccount(1000, BigDecimal.valueOf(500)));
+        mockMvc.perform(get("/bankaccount/1000/balance")).andDo(print());
+    }
+
+    @Test(expected = NotFoundException.class) /*TODO relize why nested servlet*/
+    public void balanceNotFound() throws Exception {
+        when(serviceMock.getById(1000)).thenThrow(new NotFoundException("Not found"));
         mockMvc.perform(get("/bankaccount/1000/balance")).andDo(print());
     }
 }
